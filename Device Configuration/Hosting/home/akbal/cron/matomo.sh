229189fa9d2978dd7fbd33f447df79992b8c7c5a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-26 19:14:05 +0200

    Added matomo cronjob

---
+++ Device Configuration/Hosting/home/akbal/cron/matomo.sh
@@ -0,0 +1,2 @@
+MAILTO="email@example.com"
+5 * * * * /usr/bin/php /home/akbal/sites/matomo.akbal.dev/current/console core:archive --url=https://matomo.akbal.dev/ > /home/akbal/sites/matomo.akbal.dev/logs/matomo-archive.log
